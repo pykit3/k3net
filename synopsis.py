f80f3b026877c12bbc091f53a9c7cf4ee2c1b571
Zhang Yanpo <drdr.xp@gmail.com>
2026-01-09 18:18:11 +0800

    docs: add Synopsis section with usage examples

--- synopsis.py
+++ synopsis.py
@@ -1,26 +1,3 @@
-# k3net
-
-[![Action-CI](https://github.com/pykit3/k3net/actions/workflows/python-package.yml/badge.svg)](https://github.com/pykit3/k3net/actions/workflows/python-package.yml)
-[![Documentation Status](https://readthedocs.org/projects/k3net/badge/?version=stable)](https://k3net.readthedocs.io/en/stable/?badge=stable)
-[![Package](https://img.shields.io/pypi/pyversions/k3net)](https://pypi.org/project/k3net)
-
-Utility functions for network related operation.
-
-k3net is a component of [pykit3] project: a python3 toolkit set.
-
-
-
-
-# Install
-
-```
-pip install k3net
-```
-
-# Synopsis
-
-```python
-
 #!/usr/bin/env python
 
 import k3net
@@ -56,18 +33,3 @@ if __name__ == "__main__":
     num = k3net.ip_to_num("192.168.0.1")
     print(num)  # 3232235521
     print(k3net.num_to_ip(num))  # '192.168.0.1'
-
-```
-
-#   Author
-
-Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>
-
-#   Copyright and License
-
-The MIT License (MIT)
-
-Copyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>
-
-
-[pykit3]: https://github.com/pykit3
